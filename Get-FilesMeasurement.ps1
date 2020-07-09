c19ba505a57785088aa6ceadaee8207878011826
Michal Olszak <MICHAL.OLSZAK@outlook.com>
2020-07-09 12:00:22 +0200

    major changes about function usage
    changed problem with relative paths.
    changed absolute paths to use $folderLocation = Split-Path -Parent $PSCommandPath

--- Get-FilesMeasurement.ps1
+++ Get-FilesMeasurement.ps1
@@ -103,7 +103,7 @@ function Get-FilesMeasurement
                     }
                 else
                     {
-                        throw "${$_} - This path is not correct, check it again for correctness. Maybe network issue?"
+                        write-error  "$($_)- This path is not correct, check it again for correctness. Maybe network issue?"
                     }
             })]
         [Alias("pt","source")]
@@ -121,7 +121,7 @@ function Get-FilesMeasurement
                     }
                     else
                     {
-                        throw "${$_} - This path is not correct, check it again for correctness. Maybe network issue? This should be path to file"
+                        write-error "$($_) - This path is not correct, check it again for correctness. Maybe network issue? This should be path to file"
                     }
             })]
         [ValidateNotNull()]
@@ -134,12 +134,12 @@ function Get-FilesMeasurement
         if ($pscmdlet.ParameterSetName -eq 'FilePath')
         {
             Write-Verbose -message "Reading content of: $FilePath"
-            $path = ((Get-Content -LiteralPath $FilePath).trim("'")).trim('"')
-            foreach ($item in $path)
+            $pathCheck = ((Get-Content -LiteralPath $FilePath).trim("'")).trim('"')
+            foreach ($item in $pathCheck)
             {
                 if (-not (test-path -literalPath $item))
                 {
-                    throw "Path ${$item} inside file ${$FilePath} is incorrect, or network issue"
+                    Write-Error "Path `"$($item)`" inside file `"$($FilePath)`" is incorrect, or network issue"
                 }
             }
         }
@@ -151,7 +151,7 @@ function Get-FilesMeasurement
             $getResults = (Get-ChildItem -Path $item -File -Recurse | Measure-Object -property Length -sum)
             if ($null -eq $getResults)
             {
-                Write-host "There are no files inside $item and its child containers"
+                Write-Verbose "There are no files inside $item and its child containers"
             }
             else
             {
--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,5 @@
 #in order to work, all following files have to be in one Get-FIlesMeasurement folder:
+# please adjust paths before
 #  Name
 #  ----
 #  testFolder1
@@ -7,14 +8,19 @@
 #  pathsFile.csv
 #  pathsFile.txt
 #  Test.ps1
+$folderLocation = Split-Path -Parent $PSCommandPath
 #dotsourcing function
-. ".\Get-FilesMeasurement\Get-FilesMeasurement.ps1"
+. "$folderLocation\Get-FilesMeasurement.ps1"
 #variables containing paths
-$singlePath = ".\Get-FilesMeasurement" # put here single path to folder
-$multiplePaths = '.\Get-FilesMeasurement\testFolder1', '.\Get-FilesMeasurement\testFolder2' # #put here multiple paths to folder
-$filePath = ".\Get-FilesMeasurement\pathsFile.txt" # put here path to file containg paths
+# important
+$singlePath = "$folderLocation" # put here single path to folder
+$multiplePaths = "$folderLocation\testFolder1", "$folderLocation\testFolder2" # #put here multiple paths to folder
+# put here path to file containg paths
+# WARNING - fill with proper absolute paths
+$filePath = "$folderLocation\pathsFile.txt"
 #get content of csv file here (supported only via pipeline by property name)
-$scvFilePath = (Get-Content ".\Get-FilesMeasurement\pathsFile.csv")
+#WARNING  - must be filled with propert absolute paths
+$scvFilePath = (import-csv "$folderLocation\pathsFile.csv")
 #deafult behavior
 Get-FilesMeasurement -Verbose
 #deafult behavior alias
@@ -25,20 +31,17 @@ Get-FilesMeasurement $singlePath -Verbose
 Get-FilesMeasurement $multiplePaths -Verbose
 Get-FilesMeasurement -Path $singlePath -Verbose
 Get-FilesMeasurement -Path $multiplePaths -Verbose
-Get-FilesMeasurement -FilePath ".\Get-FilesMeasurement\pathsFile.txt" -Verbose
+Get-FilesMeasurement -FilePath "$filePath" -Verbose
 # csv are not supported in file proprety
 write-host "Not supported files extension" -ForegroundColor cyan
-Get-FilesMeasurement -FilePath ".\Get-FilesMeasurement\pathsFile.csv" -Verbose
+Get-FilesMeasurement -FilePath "$scvFilePath" -Verbose
 #Support for pipe(value)
 write-host "Pipeline Support by value" -ForegroundColor Cyan
 $singlePath | Get-FilesMeasurement -Verbose
 $multiplePaths | Get-FilesMeasurement -Verbose
-#Support for pipe(property name)
+#Support for pipe(property name) - will show error if file won't be filled wil proper paths
 write-host "Pipeline support by property name" -ForegroundColor cyan
 $scvFilePath | Get-FilesMeasurement -Verbose
 #this is not allowed
 write-host "Not allowed pipeline input" -ForegroundColor cyan
 $filePath | Get-FilesMeasurement -Verbose
-
-
-Get-ChildItem -Path "C:\Workspace\DevelopmentPowerShell\Get-FilesMeasurement" | select Name
